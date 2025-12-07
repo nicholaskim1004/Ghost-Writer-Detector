7053918f54618f9fe1b75b98ecb417ff8008c2c8
nicholaskim1004 <nicholaskim.1010@gmail.com>
2025-12-06 22:17:41 -0500

    my bootstrap implementation was not correct. reworked it so now it should be real bootstraped mean and std

--- ghost_score.py
+++ ghost_score.py
@@ -123,38 +123,72 @@ for i in range(len(val_data)):
 ## performing bootstrapping on val ghost scores to get distribution for artist to artist simiarities
 num_sim = 5000
 
-samples = {}
+keys = ['Tyler, The Creator-Mac Miller', 'Tyler, The Creator-Kendrick Lamar', 'Tyler, The Creator-Quentin Miller', 'Tyler, The Creator-Drake', 'Tyler, The Creator-J. Cole', 'Tyler, The Creator-A Tribe Called Quest', 'Tyler, The Creator-Kanye West', 'Tyler, The Creator-Soulja Boy', 'Tyler, The Creator-Jay-Z', 'Tyler, The Creator-Big L', 'Tyler, The Creator-2Pac', 'Tyler, The Creator-Joey Bada$$', 
+        'A Tribe Called Quest-Mac Miller', 'A Tribe Called Quest-Kendrick Lamar', 'A Tribe Called Quest-Quentin Miller', 'A Tribe Called Quest-Drake', 'A Tribe Called Quest-J. Cole', 'A Tribe Called Quest-Kanye West', 'A Tribe Called Quest-Soulja Boy', 'A Tribe Called Quest-Jay-Z', 'A Tribe Called Quest-Big L', 'A Tribe Called Quest-Tyler, The Creator', 'A Tribe Called Quest-2Pac', 'A Tribe Called Quest-Joey Bada$$', 
+        '2Pac-Mac Miller', '2Pac-Kendrick Lamar', '2Pac-Quentin Miller', '2Pac-Drake', '2Pac-J. Cole', '2Pac-A Tribe Called Quest', '2Pac-Kanye West', '2Pac-Soulja Boy', '2Pac-Jay-Z', '2Pac-Big L', '2Pac-Tyler, The Creator', '2Pac-Joey Bada$$', 
+        'J. Cole-Mac Miller', 'J. Cole-Kendrick Lamar', 'J. Cole-Quentin Miller', 'J. Cole-Drake', 'J. Cole-A Tribe Called Quest', 'J. Cole-Kanye West', 'J. Cole-Soulja Boy', 'J. Cole-Jay-Z', 'J. Cole-Big L', 'J. Cole-Tyler, The Creator', 'J. Cole-2Pac', 'J. Cole-Joey Bada$$', 
+        'Drake-Mac Miller', 'Drake-Kendrick Lamar', 'Drake-Quentin Miller', 'Drake-J. Cole', 'Drake-A Tribe Called Quest', 'Drake-Kanye West', 'Drake-Soulja Boy', 'Drake-Jay-Z', 'Drake-Big L', 'Drake-Tyler, The Creator', 'Drake-2Pac', 'Drake-Joey Bada$$', 
+        'Kendrick Lamar-Mac Miller', 'Kendrick Lamar-Quentin Miller', 'Kendrick Lamar-Drake', 'Kendrick Lamar-J. Cole', 'Kendrick Lamar-A Tribe Called Quest', 'Kendrick Lamar-Kanye West', 'Kendrick Lamar-Soulja Boy', 'Kendrick Lamar-Jay-Z', 'Kendrick Lamar-Big L', 'Kendrick Lamar-Tyler, The Creator', 'Kendrick Lamar-2Pac', 'Kendrick Lamar-Joey Bada$$', 
+        'Mac Miller-Kendrick Lamar', 'Mac Miller-Quentin Miller', 'Mac Miller-Drake', 'Mac Miller-J. Cole', 'Mac Miller-A Tribe Called Quest', 'Mac Miller-Kanye West', 'Mac Miller-Soulja Boy', 'Mac Miller-Jay-Z', 'Mac Miller-Big L', 'Mac Miller-Tyler, The Creator', 'Mac Miller-2Pac', 'Mac Miller-Joey Bada$$', 
+        'Soulja Boy-Mac Miller', 'Soulja Boy-Kendrick Lamar', 'Soulja Boy-Quentin Miller', 'Soulja Boy-Drake', 'Soulja Boy-J. Cole', 'Soulja Boy-A Tribe Called Quest', 'Soulja Boy-Kanye West', 'Soulja Boy-Jay-Z', 'Soulja Boy-Big L', 'Soulja Boy-Tyler, The Creator', 'Soulja Boy-2Pac', 'Soulja Boy-Joey Bada$$', 
+        'Kanye West-Mac Miller', 'Kanye West-Kendrick Lamar', 'Kanye West-Quentin Miller', 'Kanye West-Drake', 'Kanye West-J. Cole', 'Kanye West-A Tribe Called Quest', 'Kanye West-Soulja Boy', 'Kanye West-Jay-Z', 'Kanye West-Big L', 'Kanye West-Tyler, The Creator', 'Kanye West-2Pac', 'Kanye West-Joey Bada$$', 
+        'Big L-Mac Miller', 'Big L-Kendrick Lamar', 'Big L-Quentin Miller', 'Big L-Drake', 'Big L-J. Cole', 'Big L-A Tribe Called Quest', 'Big L-Kanye West', 'Big L-Soulja Boy', 'Big L-Jay-Z', 'Big L-Tyler, The Creator', 'Big L-2Pac', 'Big L-Joey Bada$$', 
+        'Joey Bada$$-Mac Miller', 'Joey Bada$$-Kendrick Lamar', 'Joey Bada$$-Quentin Miller', 'Joey Bada$$-Drake', 'Joey Bada$$-J. Cole', 'Joey Bada$$-A Tribe Called Quest', 'Joey Bada$$-Kanye West', 'Joey Bada$$-Soulja Boy', 'Joey Bada$$-Jay-Z', 'Joey Bada$$-Big L', 'Joey Bada$$-Tyler, The Creator', 'Joey Bada$$-2Pac', 
+        'Quentin Miller-Mac Miller', 'Quentin Miller-Kendrick Lamar', 'Quentin Miller-Drake', 'Quentin Miller-J. Cole', 'Quentin Miller-A Tribe Called Quest', 'Quentin Miller-Kanye West', 'Quentin Miller-Soulja Boy', 'Quentin Miller-Jay-Z', 'Quentin Miller-Big L', 'Quentin Miller-Tyler, The Creator', 'Quentin Miller-2Pac', 'Quentin Miller-Joey Bada$$', 
+        'Jay-Z-Mac Miller', 'Jay-Z-Kendrick Lamar', 'Jay-Z-Quentin Miller', 'Jay-Z-Drake', 'Jay-Z-J. Cole', 'Jay-Z-A Tribe Called Quest', 'Jay-Z-Kanye West', 'Jay-Z-Soulja Boy', 'Jay-Z-Big L', 'Jay-Z-Tyler, The Creator', 'Jay-Z-2Pac', 'Jay-Z-Joey Bada$$']
+
+sim_est = {k: [] for k in keys}
+
+artist_to_indices = {
+    aid: val_data.reset_index().index[val_data["artist_id"] == aid].tolist()
+    for aid in range(13)
+}
 
 for sim in range(num_sim):
-    rand_ind = np.random.randint(0,len(val_data))
-    
-    ind_to_main = val_data.index[rand_ind]
+    samples = {k: [] for k in keys}
 
-    ref_artist = hip['artist'][ind_to_main]
-    ref_artist_id = artist_mapping[ref_artist]
-    
-    p_for_ref = pred_val[rand_ind][ref_artist_id]
-        
-    diff = pred_val[rand_ind] - p_for_ref
+    for ref_artist_id in range(13):
+        ref_name = id_to_name[ref_artist_id]
 
-    similar_score = torch.tensor([1/(1+abs(d)) for d in diff])
+        real_inds = artist_to_indices[ref_artist_id]
+        n_real = len(real_inds)
 
-    total = sum(similar_score) - 1
-    
-    p_ghost = torch.tensor([s/total for s in similar_score])
-    
-    for i, p in enumerate(p_ghost):
-        tar_artist = id_to_name[i]
-        if tar_artist == ref_artist:
-            continue
-        
-        key = f"{ref_artist}-{tar_artist}"
-        
-        samples.setdefault(key, []).append(float(p))
+        # bootstrap indices with replacement
+        boot_inds = np.random.choice(real_inds, size=n_real, replace=True)
+
+        for ind in boot_inds:
+
+            p_for_ref = pred_val[ind][ref_artist_id]
+            diff = pred_val[ind] - p_for_ref
+
+            similar_score = torch.tensor([1/(1+abs(d)) for d in diff])
+            total = sum(similar_score) - 1
+            p_ghost = torch.tensor([s/total for s in similar_score])
+
+            for i, p in enumerate(p_ghost):
+                tar_name = id_to_name[i]
+                if tar_name == ref_name:
+                    continue
+
+                key = f"{ref_name}-{tar_name}"
+                samples[key].append(float(p))
+                
+    #calculate mean and std for each art from each sample
+    for key, value in samples.items():
+        sim_est[key].append([np.mean(value), np.std(value)])
+
+
+#creating final bootstrap estimate
+df = pd.DataFrame(sim_est)
+
+boot_est = {k: 0 for k in keys}
+
+for key in keys:
+    boot_est[key] = [np.mean(df[key].str[0]), np.std(df[key].str[1])]
 
 #use these bootstrap samples to calculate mean and std for each art to art distribution
 art_to_art_info = {}
-for key, value in samples.items():
+for key, value in boot_est.items():
     art_to_art_info[key] = [np.mean(value), np.std(value)]
     
 #now will use these values to create z-scores for our test ghost scores
